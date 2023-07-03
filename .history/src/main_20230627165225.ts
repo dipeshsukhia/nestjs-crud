a50c0553ee45ec5c0248ce108ffc34c8351efb0e
dsukhia <dsukhia@entrata.com>
2023-07-03 11:03:53 +0530

    es lint fixes

--- .history/src/main_20230627165225.ts
+++ .history/src/main_20230627165225.ts
@@ -4,7 +4,7 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  
   // for globle validation
   // app.useGlobalPipes(new ValidationPipe());
   await app.listen(9000);
