c50a9534ea823229830fd9c6c319a22727327bcb
dsukhia <dsukhia@entrata.com>
2023-07-03 16:59:39 +0530

    mysql connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ItemsModule } from './items/items.module';
+import Orm from './typeorm/orm';
 
 @Module({
-  imports: [ItemsModule],
+  imports: [Orm, ItemsModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/typeorm/orm.ts
@@ -0,0 +1,15 @@
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Item } from './entities/Item';
+
+const Orm = TypeOrmModule.forRoot({
+  type: 'mysql',
+  host: 'db4free.net',
+  port: 3306,
+  username: 'hello_world',
+  password: 'hello_world',
+  database: 'hello_world',
+  entities: [Item],
+  synchronize: true,
+});
+
+export default Orm;
