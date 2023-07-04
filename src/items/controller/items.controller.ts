6d748523f7bf98253132330c8dabcb84554b0010
dsukhia <dsukhia@entrata.com>
2023-07-04 17:42:21 +0530

    async method

--- src/items/controller/items.controller.ts
+++ src/items/controller/items.controller.ts
@@ -22,57 +22,52 @@ export class ItemsController {
   constructor(private itemsService: ItemsService) {}
 
   @Get()
-  findAll(): Promise<Item[]> {
-    return this.itemsService.findAll();
+  async findAll(): Promise<Item[]> {
+    return await this.itemsService.findAll();
   }
 
   @Post()
   @UsePipes(ValidationPipe)
-  create(@Body() createItemDto: CreateItemDto): Promise<Item> {
-    return this.itemsService.create(createItemDto);
+  async create(@Body() createItemDto: CreateItemDto): Promise<Item> {
+    return await this.itemsService.create(createItemDto);
   }
 
   @Get(':id')
-  findOne(@Param('id', ParseIntPipe) id): Promise<Item> {
-    return this.itemsService.findOne(id).then((item: Item) => {
-      if (!item) {
-        throw new HttpException('Item not available!!!', HttpStatus.NOT_FOUND);
-      }
-      return item;
-    });
+  async findOne(@Param('id', ParseIntPipe) id): Promise<Item> {
+    const item = await this.itemsService.findOne(id);
+    if (!item) {
+      throw new HttpException('Item not available!!!', HttpStatus.NOT_FOUND);
+    }
+    return item;
   }
 
   @Put(':id')
   @UsePipes(ValidationPipe)
-  update(
+  async update(
     @Body() updateItemDto: UpdateItemDto,
     @Param('id', ParseIntPipe) id,
   ): Promise<Item> {
-    return this.itemsService.findOne(id).then((item: Item) => {
-      if (!item) {
-        throw new HttpException('Item not available!!!', HttpStatus.NOT_FOUND);
-      }
-      return this.itemsService.update(id, updateItemDto).then((result) => {
-        if (!result) {
-          throw new HttpException('Item not Updated!!!', HttpStatus.NOT_FOUND);
-        }
-        return this.itemsService.findOne(id);
-      });
-    });
+    const item = await this.itemsService.findOne(id);
+    if (!item) {
+      throw new HttpException('Item not available!!!', HttpStatus.NOT_FOUND);
+    }
+    const result = await this.itemsService.update(id, updateItemDto);
+    if (!result) {
+      throw new HttpException('Item not Updated!!!', HttpStatus.NOT_FOUND);
+    }
+    return await this.itemsService.findOne(id);
   }
 
   @Delete(':id')
-  delete(@Param('id', ParseIntPipe) id): Promise<Item> {
-    return this.itemsService.findOne(id).then((item: Item) => {
-      if (!item) {
-        throw new HttpException('Item not available!!!', HttpStatus.NOT_FOUND);
-      }
-      return this.itemsService.delete(id).then((result) => {
-        if (!result) {
-          throw new HttpException('Item not Deleted!!!', HttpStatus.NOT_FOUND);
-        }
-        return item;
-      });
-    });
+  async delete(@Param('id', ParseIntPipe) id): Promise<Item> {
+    const item = await this.itemsService.findOne(id);
+    if (!item) {
+      throw new HttpException('Item not available!!!', HttpStatus.NOT_FOUND);
+    }
+    const result = await this.itemsService.delete(id);
+    if (!result) {
+      throw new HttpException('Item not Deleted!!!', HttpStatus.NOT_FOUND);
+    }
+    return item;
   }
 }
